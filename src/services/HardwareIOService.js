885fa94a781afd335eda9d8fe21cfeae0712d073
Marc Peters <mr.marc.peters@gmail.com>
2017-12-31 18:00:44 -0800

    Implementing min switching frequency for hardware pins

--- src/services/HardwareIOService.js
+++ src/services/HardwareIOService.js
@@ -1,16 +1,40 @@
+import * as _ from 'lodash';
 import logger from './Logger'
 import constants from '../constants';
+import { config } from '../config';
 
 const rpio = require('rpio');
 
 class HardwareIOService {
 
+  constructor () {
+    this._pinMeta = _.fromPairs(config.OUTLETS.map(outlet => [outlet.pin, {lastSwitched: 0, state: constants.OUTLET_OFF}]));
+    console.log(this._pinMeta);
+  }
+
+
   setPinState(pinNum, state) {
+    const millisNow = new Date().getTime();
+
+    if (this._pinMeta[pinNum].state === state) {
+      // logger.debug(`Won't switch pin ${pinNum}: switching too fast`);
+      return;
+    }
+    else if(this._pinMeta[pinNum].lastSwitched + constants.OUTLET_MIN_SWITCHING_INTERVAL_MS > millisNow) {
+      logger.debug(`Won't switch pin ${pinNum}: switching too fast`);
+      return;
+    }
+
+
     switch(state) {
       case constants.OUTLET_ON:
+        this._pinMeta[pinNum].state = constants.OUTLET_ON;
+        this._pinMeta[pinNum].lastSwitched = millisNow;
         logger.debug(`Pin ${pinNum} ON`);
         break;
       case constants.OUTLET_OFF:
+        this._pinMeta[pinNum].state = constants.OUTLET_OFF;
+        this._pinMeta[pinNum].lastSwitched = millisNow;
         logger.debug(`Pin ${pinNum} OFF`);
         break;
       default:
